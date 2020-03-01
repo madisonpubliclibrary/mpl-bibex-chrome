fb3657d903be3e4158336124887d018c30953153
Lucas Schneider <lrschneider94@gmail.com>
2020-03-01 16:07:05 -0600

    v3.3 Removed unnecessary text when formatting notices

--- noticeFormatter/noticeFormatter.js
+++ noticeFormatter/noticeFormatter.js
@@ -9,8 +9,7 @@
                                 .replace(/Dear /g, "\n\n\n\n\n\n$&") // Hold notices
                                 .replace(/These items are/g, "\n\n$&") // Hold notices
                                 .replace(/Title:/g, "\n$&") // Hold notices
-                                .replace(/If\s+you\s+received\s+this\s+notice\s+via\s+email,?\s+it\s+was\s+sent\s+from\s+a\s+notification-only\s+address\s+that\s+cannot\s+accept\s+incoming\s+mail\.\s+Please\s+Do\s+Not\s+Reply\s+to\s+this\s+message\s+but\s+contact\s+your\s+local\s+library\s+directly\./g,
-                                         "\n\nIf you received this notice via email, it was sent from a notification-only address that cannot accept incoming mail. Please Do Not Reply to this message but contact your local library directly.") // All notices
+                                .replace(/If\s+you\s+received\s+this\s+notice\s+via\s+email,?\s+it\s+was\s+sent\s+from\s+a\s+notification-only\s+address\s+that\s+cannot\s+accept\s+incoming\s+mail\.\s+Please\s+Do\s+Not\s+Reply\s+to\s+this\s+message\s+but\s+contact\s+your\s+local\s+library\s+directly\./g,'') // All notices
                                 .replace(/Notice of (Item\(s\) Overdue|Overdue Item\(s\)) - \d+ day/g, "\n\n\n\n\n\n$&") // Overdue notices
                                 .replace(/2[0-9]{13}/g, "\n$&\n\n") // Overdue and >$50 notices
                                 .replace(/Library\s+records\s+show\s+the\s+following\s+item\(s\)\s+overdue\.\s+Please\s+return\s+them\s+soon\s+so\s+that\s+others\s+may\s+borrow\s+them,\s+and\s+to\s+avoid\s+further\s+charges\.\s+If\s+you\s+would\s+like\s+to\s+renew\s+your\s+items,\s+please\s+log\s+into\s+our\s+catalog\s+at\s+https:\/\/www\.linkcat\.info\/\s+Call\s+your\s+local\s+library\s+if\s+you\s+have\s+questions\s+regarding\s+this\s+notice\./g,
