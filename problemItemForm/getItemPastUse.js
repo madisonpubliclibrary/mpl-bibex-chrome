5c077d48dd11e6a25753cdc333bfaea493f68274
Lucas Schneider <lrschneider94@gmail.com>
2020-12-11 12:54:50 -0600

    v2020.12.11 Bug fix post Bibliovation upgrade; changed versioning scheme

--- problemItemForm/getItemPastUse.js
+++ problemItemForm/getItemPastUse.js
@@ -3,15 +3,15 @@
   const data = {"found": false};
 
   let itemBC = location.search.match(/mbxItemBC=3[0-9]{13}/)[0].match(/3[0-9]{13}/)[0];
-  let items = Array.from(document.querySelectorAll('.item-cell.barcode'));
+  let items = Array.from(document.querySelectorAll('.item-data-barcode'));
 
   if (items.length > 0) {
     for (let item of items) {
       if (item.textContent.includes(itemBC)) {
         data.found = true;
         data.pastUse = 0;
-        for (let col of item.parentElement.parentElement.parentElement.children) {
-          if (col.classList.contains('952.Z')) {
+        for (let col of item.parentElement.children) {
+          if (col.classList.contains('item-data-952.Z')) {
             data.pastUse = col.textContent.trim() || "0";
           }
         }
--- problemItemForm/getItemTitleCopiesHolds.js
+++ problemItemForm/getItemTitleCopiesHolds.js
@@ -26,9 +26,9 @@
         data.itemID = item.parentElement.href.match(/=\d+/)[0].substr(1);
         for (let td of item.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.children) {
 
-          if (td.classList.contains('ccode')) {
+          if (td.classList.contains('item-data-ccode')) {
             data.cCode = td.textContent.trim();
-          } else if (td.classList.contains('_availability')) {
+          } else if (td.classList.contains('item-data-_availability')) {
             if (/^Checked out/.test(td.textContent.trim().replace(/\s+/g, ' '))) {
               let checkedOut = td.textContent.trim().replace(/\s+/g, ' ').match(/^Checked out to \d+/);
               if (checkedOut.length === 1) {
